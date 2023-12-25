1112d6892ae8617cdace865ab885f596ea8b716f
Yahav Avramov <yahav99999@gmail.com>
2023-12-25 16:11:26 +0200

    removing unnecessary code

--- src/app/components/Dashboard /map/map.component.ts
+++ src/app/components/Dashboard /map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppService } from '../../../services/app.service';
 import { HttpClient } from '@angular/common/http';
 
@@ -10,19 +10,6 @@ import { HttpClient } from '@angular/common/http';
 export class MapComponent {
   constructor(private service: AppService, private http: HttpClient) {}
 
-  display: any;
   center: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
-  zoom = 4;
-
-  moveMap(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.center = event.latLng.toJSON();
-  }
-
-  move(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.display = event.latLng.toJSON();
-  }
-  countries: string[] = ['USA', 'Canada', 'Germany', 'Japan', 'Australia'];
-
-  // Marker positions based on country names
-  markers: google.maps.LatLngLiteral[] = [];
+  zoom = 3;
 }
